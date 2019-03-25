303720a3f75b575c26809f572132076454c0f829
Francois Wouts <francois.wouts@airtasker.com>
2019-03-25 13:37:06 +1100

    Revert "Ignore irrelevant headers when matching requests (#16)"
    This reverts commit 92a2aaa920e1c373a732e87da8ea6172fa5d6986.
    
    The approach was not sane: request headers are not actually used in the comparison itself, we're only looking at them to extract the `content-encoding` header.
    
    This is why you need good test coverage :)

--- src/similarity.ts
+++ src/similarity.ts
@@ -11,20 +11,29 @@ import { Headers, TapeRecord } from "./tape";
  * - +Infinity means "very different" (no match)
  */
 export function computeSimilarity(
+  requestMethod: string,
   requestPath: string,
   requestHeaders: Headers,
   requestBody: Buffer,
   compareTo: TapeRecord
 ): number {
+  if (requestMethod !== compareTo.request.method) {
+    // If the HTTP method is different, no match.
+    return +Infinity;
+  }
+  if (
+    pathWithoutQueryParameters(requestPath) !==
+    pathWithoutQueryParameters(compareTo.request.path)
+  ) {
+    // If the path is different (apart from query parameters), no match.
+    return +Infinity;
+  }
   const parsedQuery = queryString.parse(requestPath);
   const parsedCompareToQuery = queryString.parse(compareTo.request.path);
-  const serialisedRequestBody = serialiseBuffer(
-    requestBody,
-    stripExtraneousHeaders(requestHeaders)
-  );
+  const serialisedRequestBody = serialiseBuffer(requestBody, requestHeaders);
   const serialisedCompareToRequestBody = serialiseBuffer(
     compareTo.request.body,
-    stripExtraneousHeaders(compareTo.request.headers)
+    compareTo.request.headers
   );
   if (
     serialisedRequestBody.encoding === "utf8" &&
@@ -51,22 +60,11 @@ export function computeSimilarity(
   return +Infinity;
 }
 
-/**
- * Strips out headers that are likely to result in false negatives.
- */
-function stripExtraneousHeaders(headers: Headers): Headers {
-  const safeHeaders: Headers = {};
-  for (const key of Object.keys(headers)) {
-    switch (key) {
-      case "accept":
-      case "user-agent":
-      case "host":
-      case "connection":
-        // Ignore.
-        continue;
-      default:
-        safeHeaders[key] = headers[key];
-    }
+function pathWithoutQueryParameters(path: string) {
+  const questionMarkPosition = path.indexOf("?");
+  if (questionMarkPosition !== -1) {
+    return path.substr(0, questionMarkPosition);
+  } else {
+    return path;
   }
-  return safeHeaders;
 }
