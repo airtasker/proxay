bb193356dc2c2e846af1c870bcd48adbc3ffa5a0
Francois Wouts <f@zenc.io>
2019-10-24 15:57:53 +1100

    Use a global CSS reset with Emotion

--- docs/src/components/layout.tsx
+++ docs/src/components/layout.tsx
@@ -5,17 +5,49 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+import { css, Global } from "@emotion/core"
 import styled from "@emotion/styled"
+import emotionReset from "emotion-reset"
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import Header from "./header"
-import "./layout.css"
 
 const MainContainer = styled.div`
   margin: 0 auto;
   max-width: 960px;
   padding: 0px 1.0875rem 1.45rem;
   padding-top: 0;
+`
+
+const globalStyle = css`
+  html {
+    font-family: sans-serif;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1 {
+    font-size: 2em;
+    margin: 0.67em 0;
+  }
+
+  h2 {
+    font-size: 1.5em;
+  }
+
+  h3 {
+    font-size: 1.2em;
+  }
+
+  h4 {
+    font-weight: bold;
+  }
 `
 
 const Layout: React.FC<{}> = ({ children }) => {
@@ -31,6 +63,12 @@ const Layout: React.FC<{}> = ({ children }) => {
 
   return (
     <>
+      <Global
+        styles={css`
+          ${emotionReset}
+          ${globalStyle}
+        `}
+      />
       <Header siteTitle={data.site.siteMetadata.title} />
       <MainContainer>
         <main>{children}</main>
