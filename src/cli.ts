aa39fc0b721983c3cae9dcbe1662a1fedeed1eea
Michael Bell <michael.bell@airtasker.com>
2020-07-17 14:40:35 +1000

    prettified cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ import fs from "fs-extra";
 import { RecordReplayServer } from "./server";
 
 function commaSeparatedList(value: string, _dummyPrevious?: any) {
-  return value ? value.split(',') : [];
+  return value ? value.split(",") : [];
 }
 
 async function main(argv: string[]) {
@@ -19,7 +19,11 @@ async function main(argv: string[]) {
     .option("--default-tape <tape-name>", "Name of the default tape", "default")
     .option("-h, --host <host>", "Host to proxy (not required in replay mode)")
     .option("-p, --port <port>", "Local port to serve on", "3000")
-    .option("-r, --redact-headers <headers>", "Request headers to redact", commaSeparatedList)
+    .option(
+      "-r, --redact-headers <headers>",
+      "Request headers to redact",
+      commaSeparatedList
+    )
     .parse(argv);
 
   const initialMode: string = (program.mode || "").toLowerCase();
@@ -27,7 +31,7 @@ async function main(argv: string[]) {
   const defaultTapeName: string = program.defaultTape;
   const host: string = program.host;
   const port = parseInt(program.port, 10);
-  const redactHeaders: string[] = program.redactHeaders
+  const redactHeaders: string[] = program.redactHeaders;
 
   switch (initialMode) {
     case "record":
@@ -67,7 +71,7 @@ async function main(argv: string[]) {
     host,
     defaultTapeName,
     enableLogging: true,
-    redactHeaders
+    redactHeaders,
   });
   await server.start(port);
   console.log(chalk.green(`Proxying in ${initialMode} mode on port ${port}.`));
