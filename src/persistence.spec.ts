d9d0bbfe9c823f14b83d3dffaf0841f1f7a978de
Mike Bell <michael.bell@airtasker.com>
2020-07-20 12:15:04 +1000

    Update src/persistence.spec.ts
    Co-authored-by: François Wouts <francois.wouts@airtasker.com>

--- src/persistence.spec.ts
+++ src/persistence.spec.ts
@@ -97,7 +97,7 @@ describe("Redaction", () => {
       },
     };
     redactRequestHeaders(record, ["x-auth-token"]);
-    expect(record.request.headers["x-auth-token"]).not.toEqual(sensitiveData);
+    expect(record.request.headers["x-auth-token"]).toEqual("XXXX");
     expect(record.request.headers.host).toEqual(hostname);
   });
 });
