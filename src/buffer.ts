94e75588b6085322502a4187aa9be6a92e07337e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-22 07:29:51 +0800

    Delete src directory

--- src/buffer.ts
+++
@@ -1,5 +0,0 @@
-export function ensureBuffer(stringOrBuffer: string | Buffer) {
-  return typeof stringOrBuffer === "string"
-    ? Buffer.from(stringOrBuffer, "utf8")
-    : stringOrBuffer;
-}
--- src/tests/config.ts
+++
@@ -1,6 +0,0 @@
-export const PROXAY_PORT = parseInt(process.env.PROXAY_PORT || "", 10) || 4000;
-export const TEST_SERVER_PORT =
-  parseInt(process.env.TEST_SERVER_PORT || "", 10) || 4001;
-
-export const PROXAY_HOST = `http://localhost:${PROXAY_PORT}`;
-export const TEST_SERVER_HOST = `http://localhost:${TEST_SERVER_PORT}`;
